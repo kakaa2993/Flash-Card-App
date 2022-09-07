a9e49ead617f9e96efaf69ef4a0f25f82305d9e6
kakaa2993@ <kakaa2993@gmail.com>
2022-09-07 21:49:00 +0100

    target the presenting the words from the database

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from tkinter import *
+import pandas
 
 ARABIC_FONT = ("Arial", 40, "italic")
 ENGLISH_FONT = ("Arial", 60, "bold")
-BACKGROUND = "#A0D995"
+BACKGROUND_COLOR = "#B1DDC6"
 
 
 windows = Tk()
 windows.title(string="Flash Card App")
-windows.config(background=BACKGROUND)
+windows.config(background=BACKGROUND_COLOR)
 
 # Create the card
-canvas = Canvas(width=800, height=527, background=BACKGROUND, highlightthickness=0)
+canvas = Canvas(width=800, height=527, background=BACKGROUND_COLOR, highlightthickness=0)
 title = canvas.create_text(400, 150, text='Arabic', font=ARABIC_FONT,)
 word = canvas.create_text(400, 263, text='Test', font=ENGLISH_FONT,)
 
@@ -23,11 +24,11 @@ canvas.grid(column=0, row=0, columnspan=2, padx=50, pady=50)
 # Create the buttons
 
 wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, padx=150, bg=BACKGROUND, highlightthickness=0)
+wrong_button = Button(image=wrong_image, padx=150, bg=BACKGROUND_COLOR, highlightthickness=0)
 wrong_button.grid(column=0, row=1, padx=100)
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, padx=150, bg=BACKGROUND, highlightthickness=0)
+right_button = Button(image=right_image, padx=150, bg=BACKGROUND_COLOR, highlightthickness=0)
 right_button.grid(column=1, row=1, padx=100)
 
 
