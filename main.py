b7c68e9ff50f2ac4091205ca3830ae77250132f0
kakaa2993@ <kakaa2993@gmail.com>
2022-09-08 17:30:52 +0100

    rewrite the next crd generation function

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 windows = Tk()
 windows.title(string="Flash Card App")
-windows.config(background=BACKGROUND_COLOR)
+windows.config(background=BACKGROUND_COLOR, padx=50, pady=50)
 
 
 # Reading the data for the words file
 data = pandas.read_csv("./data/English Arabic - Sheet1.csv")
-english_words = data.English.to_list()
-# print(english_words)
+english_words = data.to_dict(orient="records")
+print(english_words)
 
 
-def reading_random_word():
-    random_word = random.choice(english_words)
+def next_word():
+    random_word = random.choice(english_words)["English"]
+    print(random_word)
     canvas.itemconfig(title, text="English")
     canvas.itemconfig(word, text=random_word)
 
@@ -28,22 +29,21 @@ def reading_random_word():
 canvas = Canvas(width=800, height=527, background=BACKGROUND_COLOR, highlightthickness=0)
 title = canvas.create_text(400, 150, text='Arabic', font=ARABIC_FONT,)
 word = canvas.create_text(400, 263, text='Test', font=ENGLISH_FONT,)
-
 background_image = PhotoImage(file="./images/card_front.png")
 canvas.create_image(403, 267, image=background_image)
 title = canvas.create_text(400, 150, text='Arabic', font=ARABIC_FONT)
 word = canvas.create_text(400, 300, text='test', font=ENGLISH_FONT)
-canvas.grid(column=0, row=0, columnspan=2, padx=50, pady=50)
+canvas.grid(column=0, row=0, columnspan=2)
 
 # Create the buttons
 
 wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, padx=150, bg=BACKGROUND_COLOR, highlightthickness=0, command=reading_random_word)
+wrong_button = Button(image=wrong_image, padx=150, bg=BACKGROUND_COLOR, highlightthickness=0, command=next_word)
 wrong_button.grid(column=0, row=1, padx=100)
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, padx=150, bg=BACKGROUND_COLOR, highlightthickness=0, command=reading_random_word)
+right_button = Button(image=right_image, padx=150, bg=BACKGROUND_COLOR, highlightthickness=0, command=next_word)
 right_button.grid(column=1, row=1, padx=100)
 
-
+next_word()
 windows.mainloop()
